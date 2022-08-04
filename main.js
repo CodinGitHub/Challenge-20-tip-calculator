e2085045ba1e81af3d72d3ebc9603a778dab3a70
CodinGitHub <gms2programador@gmail.com>
2022-08-04 04:02:12 -0500

    script for video

--- main.js
+++ main.js
@@ -14,9 +14,7 @@ botons.forEach((element)=>{
     
     element.addEventListener('click', ()=>{
 
-        botons.forEach(element => {
-            element.classList.remove('btns__button--selected');
-        })
+        removeActive();
 
         element.classList.add('btns__button--selected');
         tipValue = parseInt(element.innerText.slice(0, -1));
@@ -27,7 +25,7 @@ botons.forEach((element)=>{
 })
 
 let activeTip;
-let activeTipNumber;
+let activeTipNumber = 5;
 function updateActive(){
     activeTip = document.querySelector('.btns__button--selected')
     activeTipNumber = parseInt(activeTip.innerText.slice(0, -1))
@@ -43,21 +41,65 @@ function calculateTip(){
 }
 
 //Actualizando Bill
-bill.addEventListener('change', ()=>{
-    billNumber = parseFloat(bill.value)
-    calculateTip()
+bill.addEventListener('input', event=>{
+        billNumber = parseFloat(bill.value);
+        calculateTip();
 });
 
 //Acutalizando personas
-people.addEventListener('change', ()=>{
+let alert = document.querySelector('.alert')
+people.addEventListener('input', ()=>{
     peopleNumber = parseInt(people.value)
+    if (peopleNumber == 0){
+        
+        people.style.borderColor = 'red';
+        alert.classList.add('error');
+
+        // people.value = 1;
+        peopleNumber = 1;
+    }else{
+        people.style.borderColor = 'hsl(189, 41%, 97%)';
+        alert.classList.remove('error');
+    }
     calculateTip()
 })
+
+//actualizando Custom
+let custom = document.querySelector('.btns__custom');
+custom.addEventListener('input', ()=>{
+    // custom.value = '';
+    removeActive();
+})
+
+custom.addEventListener('input', event=>{
+        activeTipNumber = parseFloat(custom.value)
+        console.log(event)
+        calculateTip()
+})
+
+
 
 //Reset 
 let resetBtn = document.querySelector('.result-reset');
 resetBtn.addEventListener('click', ()=>{
+    bill.value = 0;
     billNumber = 0;
-    peopleNumber = 0;
+    people.value = '5'
+    peopleNumber = 5;
+    activeTipNumber = 5;
+    custom.value = 'Custom';
     calculateTip()
+    removeActive()
+    
+
+    botons[0].classList.add('btns__button--selected');
+    tipValue = parseInt(element.innerText.slice(0, -1));
+
+
 });
+
+function removeActive(){
+    botons.forEach(element => {
+        element.classList.remove('btns__button--selected');
+    })
+}
